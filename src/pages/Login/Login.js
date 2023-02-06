3ded3d235220b887d020290d04276c2f495c2c0a
Felipe-Gs <felipegs2905@gmail.com>
2023-02-06 15:17:47 -0300

    feat: tela login criada

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,33 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Paragraph, TextInput } from 'react-native-paper'
+import { useNavigation} from "@react-navigation/native";
+
+// import logo from '.src/imgs/logo.jpeg';
 
 export function Login() {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const {navigate} = useNavigation();
+
   return (
     <SafeAreaView style={style.container}>
+      <Image style={{width:350, height:250}} source={require('../../imgs/logo.png')}/>
       <Text style={style.textOnibus}>Onibus App</Text>
+      <View style={style.ViewInputs}>
+        <TextInput style={style.Inputs} type='outlined' label='email' value={email}  onChangeText={text => setEmail(text)}/>
+        <TextInput style={style.Inputs} type='outlined' label='senha' value={senha}  onChangeText={text => setSenha(text)}/>
+      </View>
+      <View style={{alignItems:"center", marginTop:20}}>
+        <Text>ou</Text>
+        <View style={{flexDirection:"row" }}>
+            <Text style={{marginRight:10}}>Não tem conta?</Text>
+            <TouchableOpacity onPress={()=> navigate('Home')}>
+                <Text style={{fontWeight:'bold'}}>SingUp</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
@@ -15,10 +38,21 @@ const style = StyleSheet.create({
         flex:1,
         padding:20, 
         alignItems:"center",
-        marginTop:30
+        justifyContent:'center',
     },
     textOnibus:{
-        fontSize:22,
+        fontSize:25,
+
         fontFamily: 'Roboto',
+        marginTop:15,
+        marginBottom:15
+    },
+    Inputs:{
+        width:300,
+        height:60,
+        marginTop:20
+    },
+    ViewInputs:{
+        
     }
 })
