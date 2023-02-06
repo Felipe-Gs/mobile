1765be584dc2c15fc482aade04b154c60cea3bd2
Felipe-Gs <felipegs2905@gmail.com>
2023-02-06 15:17:33 -0300

    feat: tela Home criada

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,68 @@
-import React from 'react'
-import { View, Text , StyleSheet,} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
+import { Ionicons } from '@expo/vector-icons'; 
+
+import { useNavigation} from "@react-navigation/native";
+import { Login } from '../Login/Login';
 
 
 export function Home() {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [nome, setNome] = useState('')
+
+    const {navigate} = useNavigation();
   return (
-    <View>
-        <Text>Componente Home</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+        <TouchableOpacity style={ style.arrowBack } onPress={()=> navigate('Login')}>
+            <Ionicons name="chevron-back-outline" size={28} color="black" />
+        </TouchableOpacity>
+      <Image style={{width:350, height:250,}} source={require('../../imgs/logo.png')}/>
+      <Text style={style.textOnibus}>Cadastro</Text>
+      <View style={style.ViewInputs}>
+        <TextInput style={style.Inputs} type='outlined' label='Email' value={email}  onChangeText={text => setEmail(text)}/>
+        <TextInput style={style.Inputs} type='outlined' label='Senha' value={senha}  onChangeText={text => setSenha(text)}/>
+        <TextInput style={style.Inputs} type='outlined' label='Nome' value={nome}  onChangeText={text => setNome(text)}/>
+      </View>
+      <View style={{alignItems:"center", marginTop:20}}>
+        <Button mode="outlined" width={300} onPress={() => console.log('Pressed')}>
+            Cadastrar
+        </Button>
+        <Text style={{marginTop:10}}>Verifique seus dados antes de cadastrar</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
+
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20, 
+        alignItems:"center",
+        justifyContent:'center',
+    },
+    textOnibus:{
+        fontSize:25,
+
+        fontFamily: 'Roboto',
+        marginTop:15,
+        marginBottom:15
+    },
+    Inputs:{
+        width:300,
+        height:60,
+        marginTop:20
+    },
+    ViewInputs:{
+        
+    },
+    arrowBack:{
+        position: 'absolute',
+        top: 50,
+        left: 20,
+        padding: 20,
+        zIndex:1
+    }
+})
