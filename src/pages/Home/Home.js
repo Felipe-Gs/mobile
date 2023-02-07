924ba4e95d24b6e3a0b71cb25814bbbbf2126fdb
Felipe-Gs <felipegs2905@gmail.com>
2023-02-07 14:42:21 -0300

    feat: adicionada funcao de logar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,52 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, Alert } from 'react-native'
 import { Button, TextInput } from 'react-native-paper'
 import { Ionicons } from '@expo/vector-icons'; 
 
 import { useNavigation} from "@react-navigation/native";
 import { Login } from '../Login/Login';
+import api from '../../axios/api';
 
 
 export function Home() {
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
-    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [nome, setNome] = useState("")
+    const [error, setError] = useState("")
+    
 
     const {navigate} = useNavigation();
+
+    const handleCadastro = async()=>{
+      try {
+        const response = await api.post("/inserir", 
+         {email, senha, nome}
+        );
+        setError('estudante cadastrado!')
+      } catch (error) {
+        setError('usuario nao foi cadastrado')
+      }
+    }
+
   return (
     <SafeAreaView style={style.container}>
         <TouchableOpacity style={ style.arrowBack } onPress={()=> navigate('Login')}>
             <Ionicons name="chevron-back-outline" size={28} color="black" />
         </TouchableOpacity>
-      <Image style={{width:350, height:250,}} source={require('../../imgs/logo.png')}/>
-      <Text style={style.textOnibus}>Cadastro</Text>
-      <View style={style.ViewInputs}>
-        <TextInput style={style.Inputs} type='outlined' label='Email' value={email}  onChangeText={text => setEmail(text)}/>
-        <TextInput style={style.Inputs} type='outlined' label='Senha' value={senha}  onChangeText={text => setSenha(text)}/>
-        <TextInput style={style.Inputs} type='outlined' label='Nome' value={nome}  onChangeText={text => setNome(text)}/>
-      </View>
-      <View style={{alignItems:"center", marginTop:20}}>
-        <Button mode="outlined" width={300} onPress={() => console.log('Pressed')}>
-            Cadastrar
-        </Button>
-        <Text style={{marginTop:10}}>Verifique seus dados antes de cadastrar</Text>
-      </View>
+        <Image style={{width:350, height:250,}} source={require('../../imgs/logo.png')}/>
+        <Text style={style.textOnibus}>Cadastro</Text>
+        <View style={style.ViewInputs}>
+          <TextInput style={style.Inputs} type='outlined' label='Email' value={email}  onChangeText={text => setEmail(text)}/>
+          <TextInput style={style.Inputs} type='outlined' label='Senha' value={senha}  onChangeText={text => setSenha(text)}/>
+          <TextInput style={style.Inputs} type='outlined' label='Nome' value={nome}  onChangeText={text => setNome(text)}/>
+          {error && <Text>{error}</Text>}
+        </View>
+        <View style={{alignItems:"center", marginTop:20}}>
+          <Button mode="outlined" width={300} onPress={()=>handleCadastro()}>
+              Cadastrar
+          </Button>
+          <Text style={{marginTop:10}}>Verifique seus dados antes de cadastrar</Text>
+        </View>
     </SafeAreaView>
   )
 }
