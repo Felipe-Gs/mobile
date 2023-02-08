5b5319977b51942aba9289908786e785029477a5
Felipe-Gs <felipegs2905@gmail.com>
2023-02-08 15:10:42 -0300

    feat: criado um dialog de confimarcao para  deletar estudante

--- src/components/AlertDelete.js
+++ src/components/AlertDelete.js
@@ -3,18 +3,25 @@ import React, { useState } from 'react'
 import { StyleSheet } from 'react-native';
 import { Button, Dialog, Portal } from 'react-native-paper';
 import { Text } from 'react-native';
+import { useAuth } from '../hooks/useAuth';
+import { useNavigation} from "@react-navigation/native";
 
-export function AlertDelete({ visible, setVisible }) {
+export function AlertDelete({ visible, setVisible, id }) {
+    const {navigate} = useNavigation();
+
     const hideDialog = () => setVisible(false);
-  
+    const {handleDelete, status} = useAuth();
+    if(status === true){
+        navigate('Login')
+    }
     return (
       <Portal>
         <Dialog visible={visible} onDismiss={hideDialog}>
           <Dialog.Icon icon="alert" />
           <Dialog.Title style={styles.title}>Deletar estudante</Dialog.Title>
           <Dialog.Content>
-            <Text variant="bodyMedium">So pode deletar o dono do email</Text>
-            <Button onPress={()=>alert("clicou")} mode='outlined' textColor='red'>Deletar</Button>
+            <Text style={{marginBottom:10}} variant="bodyMedium">Somente o dono do email pode deletar o seu email</Text>
+            <Button onPress={()=>handleDelete(id)} mode='outlined' textColor='red'>Deletar</Button>
           </Dialog.Content>
         </Dialog>
       </Portal>
