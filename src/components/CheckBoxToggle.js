7ac5ea5519ecf5efb2c5d8af38a9d63668a2711d
Felipe-Gs <felipegs2905@gmail.com>
2023-02-10 13:04:48 -0300

    feat: otimização na envio de dados

--- src/components/CheckBoxToggle.js
+++ src/components/CheckBoxToggle.js
@@ -8,25 +8,49 @@ import { useAuth } from '../hooks/useAuth';
 export default function CheckBoxToggle() {
   // const [checked, setChecked] = useState(false);
   const { estudante } = useAuth();
+  const [volta, setVolta] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
 
-  const [volta, setVolta] = useState(false)
+  useEffect(()=>{
+    async function getInitialValue() {
+      setLoading(true);
+
+      try {
+        const response = await api.get(`/volta/${estudante.id}`);
+        setVolta(response.data.volta);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+
+    getInitialValue();
+  },[])
 
   const handleVolta = async()=>{
+    setLoading(true);
+    setSuccess(false);
+    setError(null);
     try {
-      const response = await api.put(`/volta/${estudante.id}`, {
+      await api.put(`/volta/${estudante.id}`, {
         volta
       })
-      alert('Dados atualizados')
+      setSuccess(true);
+      setLoading(false);
     } catch (error) {
-      console.error(error)
+      setError(err);
+      setLoading(false);
     } 
   }
 
   return (
-    <View style={{width:'100%', flexDirection:'column', alignItems:"center", justifyContent:"space-around", marginTop:15}}>
-      <Text>Voce vai voltar?</Text>
-      <View style={{flexDirection:'row', alignItems:"center", marginTop:10}}>
-        <Checkbox 
+    <View style={{ width: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', marginTop: 15 }}>
+      <Text>Você vai voltar?</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+        <Checkbox
           status={volta ? 'checked' : 'unchecked'}
           onPress={() => {
             setVolta(!volta);
@@ -34,6 +58,9 @@ export default function CheckBoxToggle() {
         />
         <Button mode='outlined' onPress={handleVolta}>Enviar</Button>
       </View>
+      {loading && <Text>Carregando...</Text>}
+      {success && <Text>Dados atualizados com sucesso!</Text>}
+      {/* {error && <Text>Ocorreu um erro: {error.message}</Text>} */}
     </View>
   )
 }
